33f829333503ab56c57365fd31054c404308caab
thiliniRu <121013708+thiliniRu@users.noreply.github.com>
2023-01-02 14:21:29 +0530

    Update Sum.java

--- Sum.java
+++ Sum.java
@@ -2,6 +2,7 @@ class Sum{
     public static void main (String args[]){
         int a=3;
         int b=8;
-        System.out.println(a+b);
+        System.out.println(a+b);// sumation
+        
     }
 }
